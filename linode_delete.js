60bc53bb5346fb7a33b5d643a4ac0b5478c90f2a
Josh Margolis <jmarg@sas.upenn.edu>
2018-04-30 17:15:15 -0400

    Adding delete even though I already reimplemented, updated tester needs work, utils

---
+++ linode_delete.js
@@ -0,0 +1,15 @@
+const Linode = require('linode-api-node');
+const config = require('./config');
+
+const lnc = new Linode(config.linode.apiKey);
+
+let deleteInstances = async () => {
+    // Delete droplets
+    let res = await lnc.getLinodeInstances();
+    let nodes = res.data;
+    let deletePromises = nodes.map(async (node) => await lnc.removeLinodeInstances(node.id));
+    let results = await Promise.all(deletePromises);
+    console.log(results);
+};
+
+deleteInstances();
